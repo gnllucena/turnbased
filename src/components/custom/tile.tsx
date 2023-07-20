66ec21698b68fcd50ea81eb3e137e7cc0210f445
Gabriel Lucena <glucena@livedata.com>
2023-07-20 02:18:45 -0400

    debugging canvas width and height

--- src/components/custom/tile.tsx
+++ src/components/custom/tile.tsx
@@ -2,7 +2,7 @@
 
 import { forwardRef } from "react"
 import * as Konva from "konva"
-import { Rect } from "react-konva"
+import { Rect, Text } from "react-konva"
 
 export interface TileProps {
   id: string
@@ -14,17 +14,20 @@ export interface TileProps {
 export const Tile = forwardRef<Konva.default.Rect, TileProps>(
   ({ id, x, y, size }, ref) => {
     return (
-      <Rect
-        id={id}
-        x={x}
-        y={y}
-        width={size}
-        height={size}
-        fill={Konva.default.Util.getRandomColor()}
-        stroke="black"
-        strokeWidth={2}
-        ref={ref}
-      />
+      <>
+        <Rect
+          id={id}
+          x={x}
+          y={y}
+          width={size}
+          height={size}
+          fill={Konva.default.Util.getRandomColor()}
+          stroke="black"
+          strokeWidth={2}
+          ref={ref}
+        ></Rect>
+        <Text text={id} fontSize={40} fill="#FFF" x={x} y={y} />
+      </>
     )
   }
 )

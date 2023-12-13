9b920d57bcf694cac037b604feff68a7846d26a2
Gabriel Lucena <glucena@livedata.com>
2023-12-13 16:18:17 -0500

    starting real scroll

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-# Next Template
+## TODO
 
-A Next.js 13 template for building apps with Radix UI and Tailwind CSS.
-
-## Features
-
-- Next.js 13 App Directory
-- Radix UI Primitives
-- Tailwind CSS
-- Dark mode
-- Tailwind CSS class sorting, merging and linting.
+- onWheel to mouse 2 click
+- Command center with actions
+- Layer for players
+- Turn
+  - Turn order
+  - Camera focus on turn start
--- src/components/custom/board.tsx
+++ src/components/custom/board.tsx
@@ -8,8 +8,6 @@ import { MappingProps } from "@/app/page"
 
 import { Tile } from "./tile"
 
-const PADDING = 500
-
 interface BoardProps {
   numberOfTilesPerRow: number
   numberOfRows: number
@@ -26,46 +24,75 @@ export function Board({
   const containerRef = useRef<HTMLDivElement>(null)
   const stageRef = useRef<Konva.default.Stage>(null)
 
+  const PADDING = 500
+  const WIDTH = 3000
+  const HEIGHT = 3000
+
   return (
-    <div ref={containerRef} style={{ maxHeight: "10px" }}>
-      <Stage
-        ref={stageRef}
-        width={numberOfRows * tileSize + PADDING}
-        height={numberOfTilesPerRow * tileSize + PADDING}
-        onWheel={() => {
-          const container = containerRef.current
-          const stage = stageRef.current
-
-          if (container === null || stage === null) return
-
-          const dx = container.scrollLeft - PADDING
-          const dy = container.scrollTop - PADDING
-
-          stage.container().style.transform = `translate(${dx}px, ${dy}px)`
-
-          stage.x(-dx)
-          stage.y(-dy)
-        }}
-        onClick={() => {
-          const tile = tiles.current.get("0,0")
-
-          tile?.ref?.fill(Konva.default.Util.getRandomColor())
+    <div
+      id="scroll-container"
+      ref={containerRef}
+      style={{
+        width: "calc(100%)",
+        height: "calc(100vh)",
+        overflow: "auto",
+        border: "1px solid grey",
+      }}
+    >
+      <div
+        id="large-container"
+        style={{
+          width: `${WIDTH}px`,
+          height: `${HEIGHT}px`,
+          overflow: "hidden",
         }}
       >
-        <Layer>
-          {Array.from(tiles.current.entries()).map(([key, value], index) => (
-            <Tile
-              key={key}
-              x={value.props.x}
-              y={value.props.y}
-              size={value.props.size}
-              ref={(el) => {
-                tiles.current.set(key, { ...value, ref: el })
-              }}
-            />
-          ))}
-        </Layer>
-      </Stage>
+        <Stage
+          ref={stageRef}
+          width={window.outerWidth}
+          height={window.outerHeight}
+          onWheel={() => {
+            const container = containerRef.current
+            const stage = stageRef.current
+
+            if (container === null || stage === null) return
+
+            const dx = container.scrollLeft - PADDING
+            const dy = container.scrollTop - PADDING
+
+            stage.container().style.transform = `translate(${dx}px, ${dy}px)`
+
+            stage.x(-dx)
+            stage.y(-dy)
+          }}
+          onClick={() => {
+            const tile = tiles.current.get("0,0")
+
+            // change camera on turn start
+            // const position = tile?.ref?.getPosition()
+
+            // get id to get props on map
+            // console.log(tile?.ref?.attrs["id"])
+
+            tile?.ref?.fill(Konva.default.Util.getRandomColor())
+          }}
+        >
+          <Layer>
+            {Array.from(tiles.current.entries()).map(([key, value], index) => (
+              <Tile
+                key={key}
+                id={value.props.id}
+                x={value.props.x}
+                y={value.props.y}
+                size={value.props.size}
+                ref={(el) => {
+                  tiles.current.set(key, { ...value, ref: el })
+                }}
+              />
+            ))}
+          </Layer>
+        </Stage>
+      </div>
     </div>
   )
 }
